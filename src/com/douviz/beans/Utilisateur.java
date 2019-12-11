5a767e736b758fd0445d95d13180b48136cce7ec
douviz89 <pasgeekdutout@gmail.com>
2019-12-11 19:19:37 +0100

    Ajout de la partie DAO - étape intégration

--- src/com/douviz/beans/Utilisateur.java
+++ src/com/douviz/beans/Utilisateur.java
@@ -1,35 +1,52 @@
 package com.douviz.beans;
 
+import java.sql.Timestamp;
+
 public class Utilisateur {
-    private String nom;
-    private String email;
-    private String motDePasse;
 
-    public Utilisateur() {
+    private Long      id;
+    private String    email;
+    private String    motDePasse;
+    private String    nom;
+    private Timestamp dateInscription;
+
+    public Long getId() {
+        return id;
     }
 
-    public String getNom() {
-        return nom;
-    }
-
-    public void setNom( String nom ) {
-        this.nom = nom;
-    }
-
-    public String getEmail() {
-        return email;
+    public void setId( Long id ) {
+        this.id = id;
     }
 
     public void setEmail( String email ) {
         this.email = email;
     }
 
-    public String getMotDePasse() {
-        return motDePasse;
+    public String getEmail() {
+        return email;
     }
 
     public void setMotDePasse( String motDePasse ) {
         this.motDePasse = motDePasse;
     }
 
+    public String getMotDePasse() {
+        return motDePasse;
+    }
+
+    public void setNom( String nom ) {
+        this.nom = nom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public Timestamp getDateInscription() {
+        return dateInscription;
+    }
+
+    public void setDateInscription( Timestamp dateInscription ) {
+        this.dateInscription = dateInscription;
+    }
 }
---
+++ src/com/douviz/dao/UtilisateurDao.java
@@ -0,0 +1,11 @@
+package com.douviz.dao;
+
+import com.douviz.beans.Utilisateur;
+
+public interface UtilisateurDao {
+
+    void creer( Utilisateur utilisateur ) throws DAOException;
+
+    Utilisateur trouver( String email ) throws DAOException;
+
+}
